780d4637d28061a6cb2cb55795dc6bbf7673a491
Jennifer-42 <jennifer.charlois@gmail.com>
2024-06-29 16:48:34 +0200

    Update backend endpoint

--- client/src/view/game/navaleBattle/utils.ts
+++ client/src/view/game/navaleBattle/utils.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { TypeGrid, TypeNavaleBattle, TypeView } from "./type";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://jenniferwp.onrender.com/");
 
 const createRandomID = (length: number) => {
   let result = "";
--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const io = new Server(server, {
   cors: {
     origin:
       process.env.NODE_ENV === "production"
-        ? "https//jenniferwp.fr"
+        ? "https://jenniferwp.onrender.com/"
         : "http://localhost:3000",
   },
 });
