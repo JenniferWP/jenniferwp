c720621fd6419252e8bae82707a86d1c93c2cfe4
Jennifer-42 <jennifer.charlois@gmail.com>
2024-06-30 23:12:21 +0200

    Update potfolio home

--- client/src/view/home/index.tsx
+++ client/src/view/home/index.tsx
@@ -1,46 +1,22 @@
-import { useEffect } from "react";
-//@ts-ignore
-import Typewriter from "typewriter-effect/dist/core";
 import "./home.css";
 
-const Home = () => {
-  useEffect(() => {
-    const textHome = document.getElementById("textHome");
-
-    const typewriter = new Typewriter(textHome, {
-      delay: 30,
-    });
-
-    typewriter
-      .typeString("Bienvenue sur Jennifer WP !<br/>")
-      .pauseFor(1000)
-      .typeString("Développeuse web spécialisée en front située à Paris")
-      .pauseFor(500)
-      .deleteChars(5)
-      .changeDelay(80)
-      .typeString("Toulouse")
-      .pauseFor(500)
-      .changeDelay(30)
-      .typeString(
-        ", je suis à la recherche de mission de programmation 💻<br/>",
-      )
-      .pauseFor(1000)
-      .typeString(
-        "Vous retrouverez ici toutes les informations importantes me concernant sur mon parcours et mes expériences<br/>",
-      )
-      .pauseFor(1000)
-      .typeString("Bonne visite !")
-      .start();
-  }, []);
-
-  return (
-    <>
-      <div className={"containerHome"}></div>
-      <div className={"home"}>
-        <div id={"textHome"} className={"textHome"}></div>
+const Home = () => (
+  <>
+    <div className={"containerHome"}></div>
+    <div className={"home"}>
+      <div id={"textHome"} className={"textHome"}>
+        Bienvenue sur Jennifer WP, mon site portfolio
+        <br />
+        Développeuse web spécialisée en front située à Toulouse, je suis à la
+        recherche de nouvelles missions de programmation 💻
+        <br />
+        Vous retrouverez ici toutes les informations importantes me concernant
+        sur mon parcours et mes expériences
+        <br />
+        Bonne visite !
       </div>
-    </>
-  );
-};
+    </div>
+  </>
+);
 
 export { Home };
